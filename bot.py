4239900602afb9233e8c7973e8376268e81e1c5f
Faisal99911 <faisal8889990@gmail.com>
2026-03-17 10:54:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import asyncio
+Import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from apscheduler.schedulers.asyncio import AsyncioScheduler
